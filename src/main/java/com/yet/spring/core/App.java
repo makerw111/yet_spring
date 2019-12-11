81edaab89cf8e78e909fb29bd1398546975e0f6d
makerw111 <maker_w111@ukr.net>
2019-12-11 23:03:55 +0200

    qw

---
+++ src/main/java/com/yet/spring/core/App.java
@@ -0,0 +1,41 @@
+package com.yet.spring.core;
+
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.yet.spring.core.beans.Client;
+import com.yet.spring.core.beans.Event;
+import com.yet.spring.core.loggers.EventLogger;
+
+public class App {
+
+    private Client client;
+
+    private EventLogger eventLogger;
+    
+    public static void main(String[] args) {
+        ConfigurableApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
+        App app = (App) ctx.getBean("app");
+        
+        Event event = ctx.getBean(Event.class);
+        app.logEvent(event, "Some event for 1");
+        
+        event = ctx.getBean(Event.class);
+        app.logEvent(event, "Some event for 2");
+        
+        ctx.close();
+    }
+    
+    public App(Client client, EventLogger eventLogger) {
+ //       super();
+        this.client = client;
+        this.eventLogger = eventLogger;
+    }
+
+    private void logEvent(Event event, String msg) {
+        String message = msg.replaceAll(client.getId(), client.getFullName());
+        event.setMsg(message);
+        eventLogger.logEvent(event);
+    }
+
+}
---
+++ src/main/java/com/yet/spring/core/loggers/CacheFileEventLogger.java
@@ -0,0 +1,40 @@
+package com.yet.spring.core.loggers;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+import com.yet.spring.core.beans.Event;
+
+public class CacheFileEventLogger extends FileEventLogger {
+
+	private int cacheSize;
+	private List<Event> cache;
+
+	public CacheFileEventLogger(String filename, int cacheSize) {
+		super(filename);
+		this.cacheSize = cacheSize;
+		this.cache = new ArrayList<Event>(cacheSize);
+	}
+	
+	public void destroy() {
+		if ( ! cache.isEmpty()) {
+			writeEventsFromCache();
+		}
+	}
+
+	@Override
+	public void logEvent(Event event) {
+		cache.add(event);
+		
+		if (cache.size() == cacheSize) {
+			writeEventsFromCache();
+			cache.clear();
+		}
+	}
+
+	private void writeEventsFromCache() {
+	    cache.stream().forEach(super::logEvent);
+	}
+
+}
